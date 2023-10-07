493d26aca5ae37feac13c3f1f37bbf9c6e9c766c
KRPpang <jinjonathan123@gmail.com>
2023-10-06 23:03:30 -0700

    hw3 2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let firstNumber;
-let secondNumber;
+let firstNum;
+let secondNum;
 let step = 0;
 let Operation;
 let result = 0;
@@ -10,54 +10,59 @@ let numArrTwo = [];
 let display = document.getElementById('display');
 display.value = 0;
 function getNumber(num){
-    if(result === 0 && step === 0 || step === 1){
+    if(step === 0 || step === 1){
         numArray.push(num);
         step = 1;
-        firstNumber = Number(numArray.join(''));
-        display.value = firstNumber;
-
+        firstNum = Number(numArray.join(''));
+        display.value = firstNum;
     }
-    else if (result === 0 && step === 2){
+    else if (step === 2){
         numArrTwo.push(num);
-        secondNumber = Number(numArrTwo.join(''));
-        display.value = numArrTwo;
-    }
-    else if (result != 0){
-        numArrTwo.push(num);
-        secondNumber = Number(numArrTwo.join(''));
-
+        secondNum = Number(numArrTwo.join(''));
+        display.value = firstNum + operation + secondNum;
     }
 }
 
 function getOperator(op){
     step = 2;
     operation = op;
+    display.value = firstNum + operation
 }
 function clearDisplay(){
     display.value = 0;
-    firstNumber = null;
-    secondNUmber = null;
+    firstNum = null;
+    secondNum = null;
     step = 0;
-    operation - null;
+    operation = null;
     result = 0;
     numArray = [];
     numArrTwo = [];
 }
 const calculateEquals = () =>{
     if(operation === '+'){
-        result = firstNumber + secondNumber;
+        result = firstNum + secondNum;
         display.value = result;
+        firstNum = result;
+        numArrTwo = [];
     }
     else if(operation === '-'){
-        result = firstNumber - secondNumber;
+        result = firstNum - secondNum;
         display.value = result;
+        firstNum = result;
+        numArrTwo = [];
     }
     else if(operation === '*'){
-        result = firstNumber * secondNumber;
+        result = firstNum * secondNum;
         display.value = result;
+        firstNum = result;
+        numArrTwo = [];
     }
     else if(operation === '/'){
-        result = firstNumber / secondNumber;
+        result = firstNum / secondNum;
         display.value = result;
+        firstNum = result;
+        numArrTwo = [];
     }
+    
+    
 }
